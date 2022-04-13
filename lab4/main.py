95d143894c0a4e4458fcf65826f30c04a8d5cc51
Loloxon <nikodemko@interia.pl>
2022-04-13 11:06:55 +0100

    kinda gettin rdy

--- lab4/main.py
+++ lab4/main.py
@@ -18,6 +18,7 @@ n = 10
 min_x = -pi
 max_x = 3 * pi
 
+
 wykres_licznik=1
 
 def f(x):
@@ -53,6 +54,8 @@ x1 = np.linspace(min_x, max_x, num=30, endpoint=True)
 
 
 for i in range(5,50,5):
+    figure, axis = plot.subplots(2,2)
+    figure.suptitle("Highest difference")
     n=i
     x_pos = np.arange(min_x, max_x + 0.01, (max_x - min_x) / (n - 1))
 
@@ -81,7 +84,7 @@ for i in range(5,50,5):
     P = [0 for _ in range(n)]
     A[0][0] = -h(1)
     A[0][1] = h(1)
-    for i in range(1, n - 1):
+    for i in range(1, n - 1): #1 brzegi3
         A[i][i - 1] = h(i)
         A[i][i] = 2 * (h(i) + h(i + 1))
         A[i][i + 1] = h(i + 1)
@@ -132,7 +135,7 @@ for i in range(5,50,5):
         R = [0 for _ in range(n)]
         L[0][0] = 1
         L[0][1] = 0
-        for i in range(1, n - 1):
+        for i in range(1, n - 1):#1 brzegi2
             L[i][i - 1] = 1
             L[i][i] = 1
             # L[i][i + 1] = h(i + 1)
@@ -140,9 +143,9 @@ for i in range(5,50,5):
         L[n - 1][n - 2] = 1
         L[n - 1][n - 1] = 1
         R[0] = 0
-        for i in range(1, n - 1):
+        for i in range(1, n):
             R[i] = 2*delt(i)
-        R[n - 1] = 2*delt(n-1)
+        # R[n - 1] = 2*delt(n-1)
         B2 = np.linalg.solve(L, R)
         # print(B2)
 
@@ -166,207 +169,407 @@ for i in range(5,50,5):
                 ans.append(y[i - 1] + b2(i) * (x[j - 1] - x_pos[i - 1]) + c2(i) * (x[j - 1] - x_pos[i - 1]) ** 2)
         return ans
 
-    plot.plot(x_pos, y, 'o', X, f(X),
+    axis[0][0].plot(x_pos, y, 'o', X, f(X),
               # x_pos, f1(x_pos), '-', x_pos, f2(x_pos), '--', x_pos, f3(x_pos), '--',
               X, s(X, 2), '--',
               X, s(X, 3), '-.'
               )
-    plot.legend(['Wezly', 'Funkcja',
+    axis[0][0].legend(['Wezly', 'Funkcja',
                  # '1 stopnia', '2 stopnia', '3 stopnia',
                  '2rd?',
                  '3rd?'
                  ], loc='best')
-    plot.xlabel("x")
-    plot.ylabel("y")
-    plot.title("Wyk. "+str(wykres_licznik)+"., funkcja sklejana " + str(3) + " rzedu dla " + str(n) + " wezlow", fontweight="bold")
+    axis[0][0].set_xlabel("x")
+    axis[0][0].set_ylabel("y")
+    axis[0][0].set_title("Wyk. "+str(wykres_licznik)+"., funkcja sklejana " + str(3) + " rzedu dla " + str(n) +
+                      " wezlow rownomiernych", fontweight="bold")
     wykres_licznik+=1
+    # plot.show()
+
+    x_pos, _ = roots_chebyt(n)
+    x_pos *= ((max_x - min_x) / 2)
+    x_pos += ((max_x + min_x) / 2)
+    y = f(x_pos)
+
+    A = [[0 for i in range(n)] for j in range(n)]
+    # B = [0 for j in range(n)]
+    P = [0 for _ in range(n)]
+    A[0][0] = -h(1)
+    A[0][1] = h(1)
+    for i in range(1, n - 1):#1 brzegi3
+        A[i][i - 1] = h(i)
+        A[i][i] = 2 * (h(i) + h(i + 1))
+        A[i][i + 1] = h(i + 1)
+
+    A[n - 1][n - 2] = h(n - 1)
+    A[n - 1][n - 1] = -h(n - 1)
+    P[0] = h(1) ** 2 * delt3(1)
+    for i in range(1, n - 1):
+        P[i] = delt(i + 1) - delt(i)
+    P[n - 1] = -(h(n - 1) ** 2) * delt3(n - 3)
+
+    # for i in A:
+    #     print(i)
+    # print("P:")
+    # print(P)
+    ro = np.linalg.solve(A, P)
+    # print(ro)
+
+
+
+    def s(x, degree):
+        # def ro(i1):
+        #
+        #
+        #     # return ddf(x_pos[i]) / 6
+        #     return roo[i1]
+
+        def b(i):
+            return (y[i] - y[i - 1]) / h(i) - h(i) * (ro[i] + 2 * ro[i - 1])
+
+        def c(i):
+            return 3 * ro[i - 1]
+
+        def d(i):
+            return (ro[i] - ro[i - 1]) / h(i)
+
+        B2 = []
+
+        def b2(i):
+            # print(i)
+            return B2[i-1]
+
+        def c2(i):
+            return (b2(i+1)-b2(i))/(2*h(i))
+
+        L = [[0 for i in range(n)] for j in range(n)]
+        # B = [0 for j in range(n)]
+        R = [0 for _ in range(n)]
+        L[0][0] = 1
+        L[0][1] = 0
+        for i in range(1, n - 1):#1 brzegi2
+            L[i][i - 1] = 1
+            L[i][i] = 1
+            # L[i][i + 1] = h(i + 1)
+
+        L[n - 1][n - 2] = 1
+        L[n - 1][n - 1] = 1
+        R[0] = 0
+        for i in range(1, n):
+            R[i] = 2*delt(i)
+        # R[n - 1] = 2*delt(n-1)
+        B2 = np.linalg.solve(L, R)
+        # print(B2)
+
+
+
+        # def c2(i):
+        #     return (b2(i+1)-b2(i))/(2*h(i))
+
+        ans = []
+        # for k in range(len(x_pos)):
+        for j in range(len(x)):
+            # if x[j]
+            i = 0
+            while i < len(x_pos)-1 and x_pos[i] < x[j]:
+                i += 1
+            # i -= 1
+            if degree == 3:
+                ans.append(y[i - 1] + b(i) * (x[j - 1] - x_pos[i - 1]) + c(i) * (x[j - 1] - x_pos[i - 1]) ** 2 + d(i) * (
+                        x[j - 1] - x_pos[i - 1]) ** 3)
+            elif degree == 2:
+                ans.append(y[i - 1] + b2(i) * (x[j - 1] - x_pos[i - 1]) + c2(i) * (x[j - 1] - x_pos[i - 1]) ** 2)
+        return ans
+
+    axis[1][0].plot(x_pos, y, 'o', X, f(X),
+              # x_pos, f1(x_pos), '-', x_pos, f2(x_pos), '--', x_pos, f3(x_pos), '--',
+              X, s(X, 2), '--',
+              X, s(X, 3), '-.'
+              )
+    axis[1][0].legend(['Wezly', 'Funkcja',
+                 # '1 stopnia', '2 stopnia', '3 stopnia',
+                 '2rd?',
+                 '3rd?'
+                 ], loc='best')
+    axis[1][0].set_xlabel("x")
+    axis[1][0].set_ylabel("y")
+    axis[1][0].set_title("Wyk. "+str(wykres_licznik)+"., funkcja sklejana " + str(3) + " rzedu dla " + str(n) +
+                      " wezlow Chebysheva", fontweight="bold")
+    wykres_licznik+=1
+    
+    x_pos = np.arange(min_x, max_x + 0.01, (max_x - min_x) / (n - 1))
+    
+    # x_pos, _ = roots_chebyt(n)
+    # x_pos *= ((max_x - min_x) / 2)
+    # x_pos += ((max_x + min_x) / 2)
+    y = f(x_pos)
+    def h(i):
+        return x_pos[i] - x_pos[i - 1]
+    
+    
+    def delt(i):
+        return (y[i] - y[i - 1]) / (x_pos[i] - x_pos[i - 1])
+    
+    
+    def delt2(i):
+        return (delt(i + 1) - delt(i)) / (x_pos[i + 1] - x_pos[i - 1])
+    
+    
+    def delt3(i):
+        return (delt2(i + 1) - delt2(i)) / (x_pos[i + 2] - x_pos[i - 1])
+    
+    
+    A = [[0 for i in range(n)] for j in range(n)]
+    # B = [0 for j in range(n)]
+    P = [0 for _ in range(n)]
+    A[0][0] = -h(1)
+    A[0][1] = h(1)
+    for i in range(1, n - 1): #2 brzegi3
+        A[i][i - 1] = h(i)
+        A[i][i] = 2 * (h(i) + h(i + 1))
+        A[i][i + 1] = h(i + 1)
+    
+    A[n - 1][n - 2] = h(n - 1)
+    A[n - 1][n - 1] = -h(n - 1)
+    P[0] = 0
+    for i in range(1, n - 1):
+        P[i] = delt(i + 1) - delt(i)
+    P[n - 1] = 0
+    
+    # for i in A:
+    #     print(i)
+    # print("P:")
+    # print(P)
+    ro = np.linalg.solve(A, P)
+    # print(ro)
+    
+    
+    
+    def s(x, degree):
+        # def ro(i1):
+        #
+        #
+        #     # return ddf(x_pos[i]) / 6
+        #     return roo[i1]
+    
+        def b(i):
+            return (y[i] - y[i - 1]) / h(i) - h(i) * (ro[i] + 2 * ro[i - 1])
+    
+        def c(i):
+            return 3 * ro[i - 1]
+    
+        def d(i):
+            return (ro[i] - ro[i - 1]) / h(i)
+    
+        B2 = []
+    
+        def b2(i):
+            # print(i)
+            return B2[i-1]
+    
+        def c2(i):
+            return (b2(i+1)-b2(i))/(2*h(i))
+    
+        L = [[0 for i in range(n)] for j in range(n)]
+        # B = [0 for j in range(n)]
+        R = [0 for _ in range(n)]
+        L[0][0] = 1
+        L[0][1] = 0
+        for i in range(1, n - 1):#2 brzegi2
+            L[i][i - 1] = 1
+            L[i][i] = 1
+            # L[i][i + 1] = h(i + 1)
+    
+        L[n - 1][n - 2] = 1
+        L[n - 1][n - 1] = 1
+        R[0] = 0
+        for i in range(1, n):
+            R[i] = 2*delt(i)
+        # R[n - 1] = 2*delt(n-1)
+        B2 = np.linalg.solve(L, R)
+        # print(B2)
+    
+    
+    
+        # def c2(i):
+        #     return (b2(i+1)-b2(i))/(2*h(i))
+    
+        ans = []
+        # for k in range(len(x_pos)):
+        for j in range(len(x)):
+            # if x[j]
+            i = 0
+            while i < len(x_pos)-1 and x_pos[i] < x[j]:
+                i += 1
+            # i -= 1
+            if degree == 3:
+                ans.append(y[i - 1] + b(i) * (x[j - 1] - x_pos[i - 1]) + c(i) * (x[j - 1] - x_pos[i - 1]) ** 2 + d(i) * (
+                        x[j - 1] - x_pos[i - 1]) ** 3)
+            elif degree == 2:
+                ans.append(y[i - 1] + b2(i) * (x[j - 1] - x_pos[i - 1]) + c2(i) * (x[j - 1] - x_pos[i - 1]) ** 2)
+        return ans
+    
+    axis[0][1].plot(x_pos, y, 'o', X, f(X),
+                 # x_pos, f1(x_pos), '-', x_pos, f2(x_pos), '--', x_pos, f3(x_pos), '--',
+                 X, s(X, 2), '--',
+                 X, s(X, 3), '-.'
+                 )
+    axis[0][1].legend(['Wezly', 'Funkcja',
+                    # '1 stopnia', '2 stopnia', '3 stopnia',
+                    '2rd?',
+                    '3rd?'
+                    ], loc='best')
+    axis[0][1].set_xlabel("x")
+    axis[0][1].set_ylabel("y")
+    axis[0][1].set_title("Wyk. "+str(wykres_licznik)+"., funkcja sklejana " + str(3) + " rzedu dla " + str(n) +
+                      " wezlow rownomiernych", fontweight="bold")
+    wykres_licznik+=1
+    # plot.show()
+    
+    x_pos, _ = roots_chebyt(n)
+    x_pos *= ((max_x - min_x) / 2)
+    x_pos += ((max_x + min_x) / 2)
+    y = f(x_pos)
+    
+    A = [[0 for i in range(n)] for j in range(n)]
+    # B = [0 for j in range(n)]
+    P = [0 for _ in range(n)]
+    A[0][0] = -h(1)
+    A[0][1] = h(1)
+    for i in range(1, n - 1):#2 brzegi3
+        A[i][i - 1] = h(i)
+        A[i][i] = 2 * (h(i) + h(i + 1))
+        A[i][i + 1] = h(i + 1)
+    
+    A[n - 1][n - 2] = h(n - 1)
+    A[n - 1][n - 1] = -h(n - 1)
+    P[0] = 0
+    for i in range(1, n - 1):
+        P[i] = delt(i + 1) - delt(i)
+    P[n - 1] = 0
+    
+    # for i in A:
+    #     print(i)
+    # print("P:")
+    # print(P)
+    ro = np.linalg.solve(A, P)
+    # print(ro)
+    
+    
+    
+    def s(x, degree):
+        # def ro(i1):
+        #
+        #
+        #     # return ddf(x_pos[i]) / 6
+        #     return roo[i1]
+    
+        def b(i):
+            return (y[i] - y[i - 1]) / h(i) - h(i) * (ro[i] + 2 * ro[i - 1])
+    
+        def c(i):
+            return 3 * ro[i - 1]
+    
+        def d(i):
+            return (ro[i] - ro[i - 1]) / h(i)
+    
+        B2 = []
+    
+        def b2(i):
+            # print(i)
+            return B2[i-1]
+    
+        def c2(i):
+            return (b2(i+1)-b2(i))/(2*h(i))
+    
+        L = [[0 for i in range(n)] for j in range(n)]
+        # B = [0 for j in range(n)]
+        R = [0 for _ in range(n)]
+        L[0][0] = 1
+        L[0][1] = 0
+        for i in range(1, n - 1):#2 brzegi2
+            L[i][i - 1] = 1
+            L[i][i] = 1
+            # L[i][i + 1] = h(i + 1)
+    
+        L[n - 1][n - 2] = 1
+        L[n - 1][n - 1] = 1
+        R[0] = 0
+        for i in range(1, n):
+            R[i] = 2*delt(i)
+        # R[n - 1] = 2*delt(n-1)
+        B2 = np.linalg.solve(L, R)
+        # print(B2)
+    
+    
+    
+        # def c2(i):
+        #     return (b2(i+1)-b2(i))/(2*h(i))
+    
+        ans = []
+        # for k in range(len(x_pos)):
+        for j in range(len(x)):
+            # if x[j]
+            i = 0
+            while i < len(x_pos)-1 and x_pos[i] < x[j]:
+                i += 1
+            # i -= 1
+            if degree == 3:
+                ans.append(y[i - 1] + b(i) * (x[j - 1] - x_pos[i - 1]) + c(i) * (x[j - 1] - x_pos[i - 1]) ** 2 + d(i) * (
+                        x[j - 1] - x_pos[i - 1]) ** 3)
+            elif degree == 2:
+                ans.append(y[i - 1] + b2(i) * (x[j - 1] - x_pos[i - 1]) + c2(i) * (x[j - 1] - x_pos[i - 1]) ** 2)
+        return ans
+    
+    axis[1][1].plot(x_pos, y, 'o', X, f(X),
+                 # x_pos, f1(x_pos), '-', x_pos, f2(x_pos), '--', x_pos, f3(x_pos), '--',
+                 X, s(X, 2), '--',
+                 X, s(X, 3), '-.'
+                 )
+    axis[1][1].legend(['Wezly', 'Funkcja',
+                    # '1 stopnia', '2 stopnia', '3 stopnia',
+                    '2rd?',
+                    '3rd?'
+                    ], loc='best')
+    axis[1][1].set_xlabel("x")
+    axis[1][1].set_ylabel("y")
+    axis[1][1].set_title("Wyk. "+str(wykres_licznik)+"., funkcja sklejana " + str(3) + " rzedu dla " + str(n) +
+                      " wezlow Chebysheva", fontweight="bold")
+    wykres_licznik+=1
+    
     plot.show()
+'''
+    def comp_abs(Y):
+        ans = 0
+        for i in range(0, N):
+            idx = min_x + (max_x - min_x) * i / N
+            ans = max(ans, abs(Y[i] - f(idx)))
+        return ans
 
 
-# wykres_licznik=4
-#
-def drawFunction():
-    plot.plot(X, f(X), label='Funkcja')
-    plot.legend()
-    plot.show()
+    def comp_sqr(Y):
+        ans = 0
+        for i in range(0, N):
+            idx = min_x + (max_x - min_x) * i / N
+            ans += (Y[i] - f(idx)) ** 2
+        return ans
 
-# drawFunction()
-#
-# def Lagrange(x, x_pos):
-#     def L(k, x):
-#         prod = 1
-#         for i in range(0, n):
-#             if i != k:
-#                 prod *= (x - x_pos[i]) / (x_pos[k] - x_pos[i])
-#         return prod
-#
-#     sum = 0
-#     for k in range(0, n, 1):
-#         sum += (f(x_pos[k]) * L(k, x))
-#     return sum
-#
-#
-# def Hermit(x, x_pos):
-#     tab = [[0 for i in range(n * 2 + 1)] for j in range(n * 2 + 1)]
-#     for i in range(1, n * 2 + 1, 2):
-#         tab[i][0] = x_pos[i // 2]
-#         tab[i + 1][0] = x_pos[i // 2]
-#         tab[i][1] = f(x_pos[(i - 1) // 2])
-#         tab[i + 1][1] = f(x_pos[(i - 1) // 2])
-#         tab[i + 1][2] = df(x_pos[i // 2])
-#
-#     for i in range(1, n * 2 + 1):
-#         for j in range(2, i + 1):
-#             if i % 2 == 1 or j > 2:
-#                 tab[i][j] = (tab[i][j - 1] - tab[i - 1][j - 1]) / (x_pos[(i - 1) // 2] - x_pos[(i - j) // 2])
-#     ans = 0
-#     prod = 1
-#     for i in range(1, n * 2 + 1):
-#         ans += prod * tab[i][i]
-#         prod *= (x - tab[i][0])
-#     return ans
-#
-#
-# def drawHermitEquidistant(N):
-#     if N:
-#         axis[0].scatter(x_pos, f(x_pos), label='Węzły')
-#         axis[0].plot(X, f(X), label='Funkcja')
-#         axis[0].plot(X, Hermit(X, x_pos), label='Metora Hermita z punktami równoodległymi')
-#         axis[0].plot(X, Lagrange(X, x_pos), label='Metora Lagrange z punktami równoodległymi')
-#         axis[0].set_title("Wyk. "+str(wykres_licznik)+"., Metoda Hermita, punkty równoodległe," + str(N) + " węzłów", fontweight="bold")
-#         axis[0].set_xlabel("x")
-#         axis[0].set_ylabel("y")
-#         axis[0].legend()
-#     return Hermit(X, x_pos)
-#
-#
-# def drawHermitCheby(N):
-#     x, _ = roots_chebyt(n)
-#     x *= ((max_x - min_x) / 2)
-#     x += ((max_x + min_x) / 2)
-#
-#     if N:
-#         axis[1].scatter(x, f(x), label='Węzły')
-#         axis[1].plot(X, f(X), label='Funkcja')
-#         axis[1].plot(X, Hermit(X, x), label='Metora Hermita z punktami Chebysheva')
-#         axis[1].plot(X, Lagrange(X, x), label='Metora Lagrange z punktami Chebysheva')
-#         axis[1].set_title("Wyk. "+str(wykres_licznik)+"., Metoda Hermita, punkty Chebysheva," + str(N) + " węzłów", fontweight="bold")
-#         axis[1].set_xlabel("x")
-#         axis[1].set_ylabel("y")
-#         axis[1].legend()
-#     return Hermit(X, x)
+    d1 = comp_abs(s(X,2))
+    d2 = comp_abs(drawHermitCheby(0))
 
-#
-# def comp_abs(Y):
-#     ans = 0
-#     for i in range(0, N):
-#         idx = min_x + (max_x - min_x) * i / N
-#         ans = max(ans, abs(Y[i] - f(idx)))
-#     return ans
-#
-#
-# def comp_sqr(Y):
-#     ans = 0
-#     for i in range(0, N):
-#         idx = min_x + (max_x - min_x) * i / N
-#         ans += (Y[i] - f(idx)) ** 2
-#     return ans
-#
-#
-# ctr = 25
-#
-# min1 = min2 = 100000000
-# i1 = i2 = -1
-# print("Obliczane różnice pierwszym sposobem")
-# print("Węzły; Hermit Equidistant; Hermit Chebyshev")
-# for i in range(3, ctr + 1):
-#     n = i
-#     X = np.arange(min_x, max_x + 0.01, 0.01)
-#     x_pos = np.arange(min_x, max_x + 0.01, (max_x - min_x) / (n - 1))
-#
-#     d1 = comp_abs(drawHermitEquidistant(0))
-#     d2 = comp_abs(drawHermitCheby(0))
-#
-#     print(n, end=": ")
-#     print(d1, end="; ")
-#     print(d2)
-#
-#     if min1 > d1:
-#         min1 = d1
-#         i1 = n
-#     if min2 > d2:
-#         min2 = d2
-#         i2 = n
-#
-# print()
-# print("Najmniejsze niedokładności otrzymano dla:")
-# print(i1, ": ", min1, "; ", i2, ": ", min2, sep="")
-# print("---------------------")
-#
-# figure, axis = plot.subplots(2)
-#
-# figure.suptitle("Highest difference")
-#
-# n = i1
-# X = np.arange(min_x, max_x + 0.01, 0.01)
-# x_pos = np.arange(min_x, max_x + 0.01, (max_x - min_x) / (n - 1))
-#
-# drawHermitEquidistant(n)
-# wykres_licznik += 1
-#
-# n = i2
-# X = np.arange(min_x, max_x + 0.01, 0.01)
-# x_pos = np.arange(min_x, max_x + 0.01, (max_x - min_x) / (n - 1))
-#
-# drawHermitCheby(n)
-# wykres_licznik += 1
-#
-# plot.show()
-#
-# min1 = min2 = 100000000
-# i1 = i2 = -1
-# print("Obliczane różnice drugim sposobem")
-# print("Węzły; Hermit Equidistant; Hermit Chebyshev")
-# for i in range(3, ctr + 1):
-#     n = i
-#     X = np.arange(min_x, max_x + 0.01, 0.01)
-#     x_pos = np.arange(min_x, max_x + 0.01, (max_x - min_x) / (n - 1))
-#
-#     d1 = comp_abs(drawHermitEquidistant(0))
-#     d2 = comp_abs(drawHermitCheby(0))
-#
-#     print(n, end=": ")
-#     print(d1, end="; ")
-#     print(d2)
-#
-#     if min1 > d1:
-#         min1 = d1
-#         i1 = n
-#     if min2 > d2:
-#         min2 = d2
-#         i2 = n
-#
-# print()
-# print("Najmniejsze niedokładności otrzymano dla:")
-# print(i1, ": ", min1, "; ", i2, ": ", min2, sep="")
-# print("---------------------")
-#
-# figure, axis = plot.subplots(2)
-#
-# figure.suptitle("Squared sum")
-#
-# n = i1
-# X = np.arange(min_x, max_x + 0.01, 0.01)
-# x_pos = np.arange(min_x, max_x + 0.01, (max_x - min_x) / (n - 1))
-#
-# drawHermitEquidistant(n)
-# wykres_licznik += 1
-#
-# n = i2
-# X = np.arange(min_x, max_x + 0.01, 0.01)
-# x_pos = np.arange(min_x, max_x + 0.01, (max_x - min_x) / (n - 1))
-#
-# drawHermitCheby(n)
-# wykres_licznik += 1
-#
-# plot.show()
+    print(n, end=": ")
+    print(d1, end="; ")
+    print(d2)
+
+    if min1 > d1:
+        min1 = d1
+        i1 = n
+    if min2 > d2:
+        min2 = d2
+        i2 = n
+
+print()
+print("Najmniejsze niedokładności otrzymano dla:")
+print(i1, ": ", min1, "; ", i2, ": ", min2, sep="")
+print("---------------------")'''
+
---
+++ lab4/v2.py
@@ -0,0 +1,252 @@
+from math import pi, cos, e, sin
+import numpy as np
+import matplotlib.pyplot as plot
+import numpy.linalg
+from scipy.special import roots_chebyt
+
+# nodes_n = 10
+min_x = -pi
+max_x = 3 * pi
+
+graph_counter = 1
+
+samples = np.arange(min_x, max_x + 0.01, 0.01)
+samples_n = len(samples)
+
+
+def f(samples):
+    k = 4
+    m = 2
+    if not isinstance(samples, float):
+        return [e ** (k * cos(m * i)) for i in samples]
+    return e ** (k * cos(m * samples))
+
+
+def genEquidistant(nodes_n):
+    return np.arange(min_x, max_x + 0.01, (max_x - min_x) / (nodes_n - 1))
+
+
+def genChebyshev(nodes_n):
+    nodes, _ = roots_chebyt(nodes_n)
+    nodes *= ((max_x - min_x) / 2)
+    nodes += ((max_x + min_x) / 2)
+    return nodes
+
+
+def cubicSpline(nodes, mode):
+    y = f(nodes)
+    nodes_n = len(nodes)
+
+    def h(i):
+        return nodes[i] - nodes[i - 1]
+
+    def delt(i):
+        return (y[i] - y[i - 1]) / (nodes[i] - nodes[i - 1])
+
+    def delt2(i):
+        return (delt(i + 1) - delt(i)) / (nodes[i + 1] - nodes[i - 1])
+
+    def delt3(i):
+        return (delt2(i + 1) - delt2(i)) / (nodes[i + 2] - nodes[i - 1])
+
+    A = [[0 for i in range(nodes_n)] for j in range(nodes_n)]
+    P = [0 for _ in range(nodes_n)]
+    if mode==0:
+        A[0][0] = -h(1)
+        A[0][1] = h(1)
+    else:
+        A[0][0] = 0
+        A[0][1] = 1
+    for i in range(1, nodes_n - 1):  # 1 brzegi3
+        A[i][i - 1] = h(i)
+        A[i][i] = 2 * (h(i) + h(i + 1))
+        A[i][i + 1] = h(i + 1)
+
+    if mode==0:
+        A[nodes_n - 1][nodes_n - 2] = h(nodes_n - 1)
+        A[nodes_n - 1][nodes_n - 1] = -h(nodes_n - 1)
+    else:
+        A[nodes_n - 1][nodes_n - 2] = 1
+        A[nodes_n - 1][nodes_n - 1] = 0
+    P[0] = h(1) ** 2 * delt3(1)
+    for i in range(1, nodes_n - 1):
+        P[i] = delt(i + 1) - delt(i)
+    P[nodes_n - 1] = -(h(nodes_n - 1) ** 2) * delt3(nodes_n - 3)
+
+    ro = np.linalg.solve(A, P)
+
+    def b(i):
+        return (y[i] - y[i - 1]) / h(i) - h(i) * (ro[i] + 2 * ro[i - 1])
+
+    def c(i):
+        return 3 * ro[i - 1]
+
+    def d(i):
+        return (ro[i] - ro[i - 1]) / h(i)
+
+    ans = []
+    for j in range(samples_n):
+        i = 0
+        while i < len(nodes) - 1 and nodes[i] < samples[j]:
+            i += 1
+        ans.append(y[i - 1] + b(i) * (samples[j - 1] - nodes[i - 1]) + c(i) * (samples[j - 1] - nodes[i - 1]) ** 2 +
+                   d(i) * (samples[j - 1] - nodes[i - 1]) ** 3)
+    return ans
+
+
+def quadriSpline(nodes, mode):
+    y = f(nodes)
+    nodes_n = len(nodes)
+
+    def h(i):
+        return nodes[i] - nodes[i - 1]
+
+    def delt(i):
+        return (y[i] - y[i - 1]) / (nodes[i] - nodes[i - 1])
+
+    B2 = []
+
+    def b2(i):
+        return B2[i - 1]
+
+    def c2(i):
+        return (b2(i + 1) - b2(i)) / (2 * h(i))
+
+    L = [[0 for i in range(nodes_n)] for j in range(nodes_n)]
+    R = [0 for _ in range(nodes_n)]
+    if mode == 0:
+        L[0][0] = 1
+        L[0][1] = 0
+    else:
+        L[0][0] = 1
+        L[0][1] = -1
+    for i in range(1, nodes_n - 1):  # 1 brzegi2
+        L[i][i - 1] = 1
+        L[i][i] = 1
+
+    L[nodes_n - 1][nodes_n - 2] = 1
+    L[nodes_n - 1][nodes_n - 1] = 1
+    R[0] = 0
+    for i in range(1, nodes_n):
+        R[i] = 2 * delt(i)
+    B2 = np.linalg.solve(L, R)
+
+    ans = []
+    for j in range(samples_n):
+        i = 0
+        while i < len(nodes) - 1 and nodes[i] < samples[j]:
+            i += 1
+        ans.append(y[i - 1] + b2(i) * (samples[j - 1] - nodes[i - 1]) + c2(i) * (samples[j - 1] - nodes[i - 1]) ** 2)
+    return ans
+
+def showGraph(nodes_n, x, y, graph_counter):
+    nodes = genEquidistant(nodes_n)
+    if y == 0:
+        if x == 0:
+            axis[x][y].plot(nodes, f(nodes), 'o', samples, f(samples), samples, quadriSpline(nodes, 0), '-.')
+        else:
+            axis[x][y].plot(nodes, f(nodes), 'o', samples, f(samples), samples, cubicSpline(nodes, 1), '-.')
+        name = "f. 2. rzedu"
+    else:
+        if x == 0:
+            axis[x][y].plot(nodes, f(nodes), 'o', samples, f(samples), samples, quadriSpline(nodes, 0), '-.')
+        else:
+            axis[x][y].plot(nodes, f(nodes), 'o', samples, f(samples), samples, cubicSpline(nodes, 1), '-.')
+        name = "f. 3. rzedu"
+
+    axis[x][y].legend(['Wezly', 'Funkcja', name], loc='best')
+    axis[x][y].set_xlabel("x")
+    axis[x][y].set_ylabel("y")
+    axis[x][y].set_title("Wyk. " + str(graph_counter) + "., funkcja sklejana, " + str(x) + ". brzegi dla " + str(nodes_n) +
+                         " wezlow rownomiernych", fontweight="bold")
+    graph_counter += 1
+    return graph_counter
+
+def comp_abs(Y):
+    ans = 0
+    for i in range(0, samples_n):
+        idx = min_x + (max_x - min_x) * i / samples_n
+        ans = max(ans, abs(Y[i] - f(idx)))
+    return ans
+
+def comp_sqr(Y):
+    ans = 0
+    for i in range(0, samples_n):
+        idx = min_x + (max_x - min_x) * i / samples_n
+        ans += (Y[i] - f(idx)) ** 2
+    return ans
+
+print("n ",
+      "quadri equidist 0; ",
+      # "quadri chebys 0; ",
+      "quadri equidist 1; ",
+      # "quadri chebys 1;   ",
+      "cubic equidist 0;   ",
+      # "cubic chebys 0;   ",
+      "cubic equidist 1;   ",
+      # "cubic chebys 1   "
+      )
+min1=min3=min5=min7=-1
+for i in range(5,100):
+    d1 = comp_abs(quadriSpline(genEquidistant(i),0))
+    # d2 = comp_abs(quadriSpline(genChebyshev(i),0))
+    d3 = comp_abs(quadriSpline(genEquidistant(i),1))
+    # d4 = comp_abs(quadriSpline(genChebyshev(i),1))
+    d5 = comp_abs(cubicSpline(genEquidistant(i),0))
+    # d6 = comp_abs(cubicSpline(genChebyshev(i),0))
+    d7 = comp_abs(cubicSpline(genEquidistant(i),1))
+    # d8 = comp_abs(cubicSpline(genChebyshev(i),1))
+
+    print(i, end=": ")
+    print(d1, end="; ")
+    # print(d2, end="; ")
+    print(d3, end="; ")
+    # print(d4, end="; ")
+    print(d5, end="; ")
+    # print(d6, end="; ")
+    print(d7)
+    # print(d8)
+
+    # figure, axis = plot.subplots(2, 2)
+    # figure.suptitle("Highest difference")
+    # graph_counter = showGraph(10, 0, 0, graph_counter, 0)
+    # # graph_counter = showGraph(10, 1, 0, graph_counter, 0)
+    # graph_counter = showGraph(10, 0, 1, graph_counter, 0)
+    # # graph_counter = showGraph(10, 1, 1, graph_counter, 0)
+    # plot.show()
+    if min1 > d1 or min1==-1:
+        min1 = d1
+        i1 = i
+    if min3 > d3 or min3==-1:
+        min3 = d3
+        i3 = i
+    if min5 > d5 or min5 == -1:
+        min5 = d5
+        i5 = i
+    if min7 > d7 or min7 == -1:
+        min7 = d7
+        i7 = i
+
+    if i==100-1:
+        print()
+        print("Najmniejsze niedokładności otrzymano dla:")
+        print(i1, ": ", min1, "; ", i3, ": ", min3, "; ", i5, ": ", min5, "; ", i7, ": ", min7, sep="")
+        print("---------------------")
+        figure, axis = plot.subplots(2, 2)
+        figure.suptitle("Wykresy")
+        graph_counter = showGraph(i1, 0, 0, graph_counter)
+        graph_counter = showGraph(i3, 1, 0, graph_counter)
+        graph_counter = showGraph(i5, 0, 1, graph_counter)
+        graph_counter = showGraph(i7, 1, 1, graph_counter)
+        plot.show()
+
+
+
+# figure, axis = plot.subplots(2,2)
+# figure.suptitle("Wykresy")
+# graph_counter = showGraph(10, 0, 0, graph_counter)
+# graph_counter = showGraph(10, 1, 0, graph_counter)
+# graph_counter = showGraph(10, 0, 1, graph_counter)
+# graph_counter = showGraph(10, 1, 1, graph_counter)
+# plot.show()
+
